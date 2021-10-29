ef156932e62a357693343c2e84f51e18d5a0a16f
Dejene <dejenedaba269@gmail.com>
2021-10-29 09:51:09 +0200

    implimented axios

--- dist/js/async.js
+++ dist/js/async.js
@@ -6,32 +6,30 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 
 var jokes = document.querySelector("#new-jokes");
 var clickForNewJokes = document.querySelector("#findNewJokes");
-var listOfJocke = ["I went to the store to pick up eight cans of sprite... when I got home I realized I'd only picked seven up", "Why did the m&m go to school? Because it wanted to be a Smartie!", "I got fired from a florist, apparently I took too many leaves."];
+var listOfJocke = [];
 
 var myFunction = /*#__PURE__*/function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-    var response, data;
+    var config, response;
     return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
-            _context.next = 2;
-            return fetch("https://icanhazdadjoke.com/", {
+            config = {
+              method: "get",
+              url: "https://icanhazdadjoke.com/",
               headers: {
                 Accept: "application/json"
               }
-            });
+            };
+            _context.next = 3;
+            return axios(config);
 
-          case 2:
+          case 3:
             response = _context.sent;
-            _context.next = 5;
-            return response.json();
+            return _context.abrupt("return", response);
 
           case 5:
-            data = _context.sent;
-            return _context.abrupt("return", data);
-
-          case 7:
           case "end":
             return _context.stop();
         }
@@ -46,24 +44,14 @@ var myFunction = /*#__PURE__*/function () {
 
 function getJock() {
   myFunction().then(function (data) {
+    console.log(data);
+    data = data.data;
     jokes.textContent = data.joke;
   });
 }
 
 clickForNewJokes.addEventListener("click", getJock);
-getJock();
-/* const url = "https://swapi.dev/api/";
-fetch(url).then((data) => console.logo(data));
-
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => console.log(data)); */
-
-/* const url = "https://swapi.dev/api/";
-fetch(url)
-  .catch((error) => console.log("BAD", error))
-  .then((response) => console.log("GOOD", response)); */
-//swipe js
+getJock(); //swipe js
 
 document.querySelector("ul").addEventListener("click", function (e) {
   var li = e.target.closest("li");
